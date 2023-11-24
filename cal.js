30e5631c59858d1f53bbf238c3bd9db396a551c8
Kreativepardha <116539060+Kreativepardha@users.noreply.github.com>
2023-11-24 10:38:42 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,35 @@
+const buttons=document.querySelectorAll(".button");
+let string="";
+let input=document.getElementById("input");
+
+
+function changein(){
+    input.value=string;
+}
+Array.from(buttons).forEach(buttons=>{
+    buttons.addEventListener("click",()=>{
+        if(buttons.innerHTML == "="){
+            string=eval(string);
+            changein();
+        }
+        else if(buttons.innerHTML==="c"){
+
+            string="";
+            changein();
+        }
+    
+        else{
+        string = string+buttons.innerHTML;
+       changein();
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
